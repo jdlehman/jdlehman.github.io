dcbcbcf09f7fa1beab77b037277c6ed2810df530
jdlehman <jonathan.lehman91@gmail.com>
2014-05-19 12:05:12 -0400

    Move Sass assets to bower
    This removes the compass dependency and cleans up the Gemfile.
    Use Sass to compile scss instead of compass by adding the
    bower components to the sass path. This hooks into Sass compilation by
    Jekyll.

--- Gemfile
+++ Gemfile
@@ -1,8 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'jekyll', '~> 2.0'
-gem 'susy'
-gem 'bourbon'
-gem 'compass', git: 'git@github.com:chriseppstein/compass.git'
 gem 'sass', '~> 3.3'
-gem 'breakpoint'
---
+++ _plugins/sass_path.rb
@@ -0,0 +1,10 @@
+require 'sass'
+
+base_directory  = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+bower_path      = File.join(base_directory, 'app/bower_components')
+
+if ENV.has_key?("SASS_PATH")
+    ENV["SASS_PATH"] = ENV["SASS_PATH"] + File::PATH_SEPARATOR + bower_path
+else
+    ENV["SASS_PATH"] = bower_path
+end
