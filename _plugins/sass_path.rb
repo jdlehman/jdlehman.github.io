707d0cb017e17597f84f54d7ad0f02dfbe967621
jdlehman <jonathan.lehman91@gmail.com>
2014-05-19 13:31:26 -0400

    Move bower components to _app
    This will prevent them being included in the jekyll build in _site.

--- _plugins/sass_path.rb
+++ _plugins/sass_path.rb
@@ -1,7 +1,7 @@
 require 'sass'
 
 base_directory  = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-bower_path      = File.join(base_directory, 'app/bower_components')
+bower_path      = File.join(base_directory, '_app/bower_components')
 
 if ENV.has_key?("SASS_PATH")
     ENV["SASS_PATH"] = ENV["SASS_PATH"] + File::PATH_SEPARATOR + bower_path
