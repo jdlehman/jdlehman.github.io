e7fe6ba03bd0f0b93bb74adaaddba0d1b4bf3203
jdlehman <jonathan.lehman91@gmail.com>
2014-05-22 22:07:17 -0400

    Move bower components path
    Ignore bower components from build as well as bower.json.

--- _plugins/sass_path.rb
+++ _plugins/sass_path.rb
@@ -1,5 +1,5 @@
 base_directory  = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-bower_path      = File.join(base_directory, '_app/bower_components')
+bower_path      = File.join(base_directory, 'bower_components')
 
 if ENV.has_key?("SASS_PATH")
     ENV["SASS_PATH"] = ENV["SASS_PATH"] + File::PATH_SEPARATOR + bower_path
